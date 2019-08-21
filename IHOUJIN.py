376c37558077b960629da101b0f8a6907c02c5b4
ToshikiMuromachi <ageki104@gmail.com>
2019-08-22 00:24:44 +0900

    相槌部分と音声合成部分が完成。音声合成部分が強すぎるので、チューニングが必要

--- IHOUJIN.py
+++ IHOUJIN.py
@@ -1,5 +1,4 @@
 # プロット関係のライブラリ
-
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
@@ -9,6 +8,7 @@ import sys
 import pyaudio
 import struct
 import winsound
+import wave
 
 # 制御関連
 import random
@@ -16,6 +16,8 @@ import platform
 import threading
 import time
 import concurrent.futures
+import subprocess
+import socket
 
 np.set_printoptions(threshold=np.inf)
 
@@ -144,9 +146,105 @@ class x12345:
             print("x12345 : " + str(self.x))
             time.sleep(0.5)
 
+class Julius:
+    def __init__(self):
+        self.host = '127.0.0.1'  # localhost
+        self.port = 10500  # julisuサーバーモードのポート
+        juliusThread = threading.Thread(target=self.main)
+        juliusThread.start()
+
+    def main(self):
+        # p = subprocess.Popen(["./run-win-dnn-module.bat"], stdout=subprocess.PIPE, shell=True) # julius起動スクリプトを実行
+        # pid = str(p.stdout.read().decode('utf-8')) # juliusのプロセスIDを取得
+        # juliusProcess = subprocess.run("run-win-dnn-module.bat", shell=True)
+
+        time.sleep(3)  # 3秒間スリープ
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((self.host, self.port))  # サーバーモードで起動したjuliusに接続
+
+        res = ''
+        startTime = 0
+        nextTime = int(random.uniform(10, 20))
+        recentlyWord = ''
+        message = ''
+        while True:
+            startTime += 1
+            # 音声認識の区切りである「改行+.」がくるまで待つ
+            while (res.find('\n.') == -1):
+                # Juliusから取得した値を格納していく
+                res += client.recv(1024).decode('shift-jis')
+
+            word = ''
+            for line in res.split('\n'):
+                # Juliusから取得した値から認識文字列の行を探す
+                index = line.find('WORD=')
+                # 認識文字列があったら...
+                if index != -1:
+                    # 認識文字列部分だけを抜き取る
+                    line = line[index + 6: line.find('"', index + 6)]
+                    # 文字列の開始記号以外を格納していく
+                    if line != '[s]':
+                        word = word + line
+
+                # 認識された文字がある場合
+                if len(word) != 0:
+                    # print("word : "+word)
+                    recentlyWord = word
+
+                # nextTimeを超過した場合
+                if startTime >= nextTime:
+                    print("音声合成 : " + recentlyWord)
+                    nextTime = nextTime + int(random.uniform(10, 20))
+                    message = recentlyWord
+                    audioStartThread = threading.Thread(target=self.voice, args=(message,))
+                    audioStartThread.start()
+
+                res = ''
+                # print("TIME : " + str(startTime) + "NEXT -> " + str(nextTime))
+
+    def voice(self, message):
+        ms = 'ててふぉふぉふぉふぉふぉて'
+        # 音声合成
+        path = [
+            'wsl echo \'',
+            message,
+            '\' | ',
+            'wsl open_jtalk',
+            ' -x /var/lib/mecab/dic/open-jtalk/naist-jdic',
+            ' -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice',
+            ' -ow /mnt/c/home/OpenJTalk/voice.wav',
+            ' -r 1.0',
+            ' -a 0.3',
+            ' -jf 1.0',
+            ' -fm -5.0', ]
+        path = ''.join(path)
+        result = subprocess.run(path, shell=True)
+
+        # 音声再生
+        time.sleep(0.1)
+        wf = wave.open('C:\\home\\OpenJTalk\\voice.wav', "r")
+
+        # ストリームを開く
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        # 音声を再生
+        chunk = 1024
+        data = wf.readframes(chunk)
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(chunk)
+        stream.close()
+        p.terminate()
+
 
 if __name__ == "__main__":
     plotwin = PlotWindow()
-    xxxxxxxx = x12345()
+    juliusUtterance = Julius()
+    #xxxxxxxx = x12345()
+
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+         QtGui.QApplication.instance().exec_()
